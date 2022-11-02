847caef1a0609c51a9a8e6732568e23f6204f00e
Alexander Dementyev <beeeblebrox@yandex.ru>
2022-11-02 22:36:00 +0300

    create files

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+/* Vars */
+
+const srcFolder = 'src'
+const distFolder = 'dist'
+
+/* Modules */
+
+import pkg from 'gulp'
+const { gulp, series, parallel, src, dest, watch } = pkg
+
+import rename     from 'gulp-rename'
+import translit from 'speakingurl'
+import {deleteAsync} from 'del'
+
+/* Rename */
+
+function rename() {
+  return src(`${srcFolder}/**/*.*`)
+  .pipe(rename(function(path){
+
+    let trlFileName = translit(path.basename, {
+      lang: 'en'
+    })
+
+    let trlDirName = translit(path.dirname, {
+      lang: 'en'
+    })
+
+    path.basename = trlFileName
+    path.dirname = trlDirName
+    // path.dirname: "main/text/ciao"
+    // path.basename: "aloha"
+    // path.prefix: "test-"
+    // path.suffix: "-test"
+    // path.extname: ".md"
+  }))
+  .pipe(dest(distFolder))
+}
+
+/* Clear */
+
+function clear() {
+  return deleteAsync([distFolder])
+}
+
+/* Exports */
+
+export { rename, clear }
+export default series(await clear, rename)
